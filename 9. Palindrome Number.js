a4ce6b168121999b10776acba55e74d01c31282a
Mahmoud-Magdy-deeplearning <mahmoudmagdymahmoud1@gmail.com>
2022-05-13 13:30:15 +0200

    Binary search

--- 9. Palindrome Number.js
+++ 9. Palindrome Number.js
@@ -3,14 +3,14 @@
  * @return {boolean}
  */
  var isPalindrome = function(x) {
-    const number  = x.toString();
-    for (let i = 0; i < number.length; i++) {
-      if(number[i]!==number[number.length-1-i]) return false
-      if(i===number.length - i - 1) break
-    }
-    return true
-  };
-  
-  const x = 121
-  console.log(isPalindrome(x))
-  // Output = true
+  const number  = x.toString();
+  for (let i = 0; i < number.length; i++) {
+    if(number[i]!==number[number.length-1-i]) return false
+    if(i===number.length - i - 1) break
+  }
+  return true
+};
+
+const x = 121
+console.log(isPalindrome(x))
+// Output = true
